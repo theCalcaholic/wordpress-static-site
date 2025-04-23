8d6b9f62ae14eea7ca49fe85c8a6dedc481babfd
Tobias Knöppler <6317548+theCalcaholic@users.noreply.github.com>
2025-04-23 14:21:30 +0200

    Add init.sh

---
+++ init.sh
@@ -0,0 +1,87 @@
+#!/usr/bin/bash
+
+set -ex
+
+read -r -p "Enter the intended domain for your static site: " DOMAIN
+read -r -p "Enter your admin (wordpress) domain (keep empty for default 'admin.${DOMAIN}'): " ADMIN_DOMAIN
+ADMIN_DOMAIN="${ADMIN_DOMAIN:-"admin.${DOMAIN}"}"
+read -r -p "Enter your matomo domain (keep empty for default 'matomo.${DOMAIN}'): " MATOMO_DOMAIN
+MATOMO_DOMAIN="${MATOMO_DOMAIN:-"matomo.${DOMAIN}"}"
+read -r -p "Enter your comentario domain (keep empty for default 'comentario.${DOMAIN}'): " COMMENTS_DOMAIN
+COMMENTS_DOMAIN="${COMMENTS_DOMAIN:-"comments.${DOMAIN}"}"
+
+while true
+do
+  read -r -N 1 -p "Do you want to use self signed certificates (e.g. for local testing or with dummy domains)? (y/N) " local_setup
+  local_setup="${local_setup,,}"
+  if [[ "${local_setup}" == y ]]
+  then
+   TLS_CONFIG_OVERRIDE="  tls internal {
+    on_demand
+  }"
+   break
+ elif [[ "${local_setup}" == n ]]
+ then
+   TLS_CONFIG_OVERRIDE=""
+   break
+ fi
+ echo "Invalid choice! Please enter y (yes) or n (no)."
+done
+
+read -r -p "Enter a username for basic authentication: " BASIC_AUTH_USER
+read -r -p "Enter a password for basic authentication: " BASIC_AUTH_PW
+
+CONTAINER_CLI=""
+if docker version > /dev/null 2>&1
+then
+  CONTAINER_CLI="docker"
+elif podman version > /dev/null 2>&1
+then
+  CONTAINER_CLI="podman"
+fi
+
+if [[ -z "$CONTAINER_CLI" ]]
+then
+  echo "No container runtime found! Please install docker or podman and make sure their command line interface is in the PATH."
+  exit 1
+fi
+
+BASIC_AUTH_PW_HASH="$("${CONTAINER_CLI}" run --rm docker.io/library/caddy:2 caddy hash-password --plaintext "${BASIC_AUTH_PW}")"
+
+POSTGRES_PASSWORD="$(base64 < /dev/urandom | head -c 32)"
+MATOMO_DB_PASSWORD="$(base64 < /dev/urandom | head -c 32)"
+
+cat <<EOF > .env
+DOMAIN="${DOMAIN?}"
+ADMIN_DOMAIN="${ADMIN_DOMAIN?}"
+MATOMO_DOMAIN="${MATOMO_DOMAIN?}"
+COMMENTS_DOMAIN="${COMMENTS_DOMAIN?}"
+
+DB_ROOT_PASSWORD="$(base64 < /dev/urandom | head -c 32)"
+WORDPRESS_DB_PASSWORD="$(base64 < /dev/urandom | head -c 32)"
+MATOMO_DB_PASSWORD="${MATOMO_DB_PASSWORD}"
+POSTGRES_PASSWORD="${POSTGRES_PASSWORD?}"
+
+BASIC_AUTH_USER="${BASIC_AUTH_USER?}"
+BASIC_AUTH_PW_HASH="${BASIC_AUTH_PW_HASH?}"
+
+TLS_CONFIG_OVERRIDE="${TLS_CONFIG_OVERRIDE}"
+EOF
+
+mkdir -p ./comentario
+cat <<EOF > ./comentario/secrets.yaml
+postgres:
+  host:     comments-db
+  port:     5432
+  database: comentario
+  username: postgres
+  password: "${POSTGRES_PASSWORD?}"
+EOF
+
+mkdir -p wordpress-db/init.d
+cat <<EOF > wordpress-db/init.d/matomo.sql
+CREATE DATABASE matomo;
+CREATE USER 'matomo' IDENTIFIED BY '${MATOMO_DB_PASSWORD}';
+GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, INDEX, DROP, ALTER, CREATE TEMPORARY TABLES, LOCK TABLES ON matomo.* TO 'matomo';
+GRANT FILE ON *.* TO 'matomo';
+EOF
